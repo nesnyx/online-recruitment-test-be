c9f2931b8eb7e26064dbcdfe335e9bad0e34e304
Aris Setiawan Bara <baraaris2@gmail.com>
2026-01-31 02:31:58 +0000

    feat: integrate sequelize into user service and enhance logging across application modules

--- src/workers/container.worker.ts
+++
@@ -1,6 +0,0 @@
-import { userService } from "../container";
-import { redisConnection } from "../utils/redis-connection";
-import { ExamWorker } from "./exam.worker";
-
-
-export const examWorker = new ExamWorker(userService,redisConnection)
